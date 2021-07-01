c0c3feb419745b9fa00965bb38ab1169b845d859
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-07-01 14:35:18 -0300

    Separando a lógica do jogo do front

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,43 +1,54 @@
-const FRONT = "card_front"
-const BACK = "card_back"
-
-let techs = ['bootstrap',
-    'css',
-    'electron',
-    'firebase',
-    'html',
-    'javascript',
-    'jquery',
-    'mongo',
-    'node',
-    'react'];
+const FRONT = "card_front";
+const BACK = "card_back";
+const CARD = "card";
+const ICON = "icon";
 
 
 
-creatCardsFromTechs (techs)
-function creatCardsFromTechs (techs) {
-    let cards = [];
+startGame ();
 
-    for (let tech of techs) {
-        cards.push(createPairFromTech(tech));
+function startGame () {
+    initializeCards(game.creatCardsFromTechs ());
+}
+
+function initializeCards (cards) {
+    let gameBoard = document.getElementById("gameBoard");
+
+    game.cards.forEach (card => {
+        let cardElement = document.createElement('div');
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+
+        createCardContent(card, cardElement)
+
+        cardElement.addEventListener ('click', flipCard);
+        gameBoard.appendChild(cardElement);
+    })
+}
+
+function createCardContent(card, cardElement) {
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
+
+}
+
+function createCardFace (face, card, element) {
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face);
+
+    if (face === FRONT) {
+        let iconElement = document.createElement ('img');
+        iconElement.classList.add(ICON);
+        iconElement.src = "./assets/images/" + card.icon + ".png";
+        cardElementFace.appendChild(iconElement);
+    } else {
+        cardElementFace.innerHTML = "&lt/&gt";
     }
 
-    return cards.flatMap(pair => pair);
+    element.appendChild(cardElementFace);
 }
 
-function createPairFromTech(tech) {
-
-    return [{
-        id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false,
-    }, {
-        id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false,
-    }]
-}
-
-function createIdWithTech(tech) {
-    return tech + parseInt(Math.random() * 1000);
+function flipCard (){
+    this.classList.add("flip");
 }
