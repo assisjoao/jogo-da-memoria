75a076d724f0ff531f922ecb7ed382f36261cacb
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-07-01 15:38:46 -0300

    Finalizado o jogo

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,4 +1,9 @@
 let game = {
+
+    lockMode : false,
+    firstCard : null,
+    secondCard : null,
+
     techs : ['bootstrap',
     'css',
     'electron',
@@ -11,6 +16,51 @@ let game = {
     'react'],
 
     cards : null,
+
+    setCard : function(id){
+
+        let card = this.cards.filter(card => card.id === id)[0];
+        
+        if (card.flipped || this.lockMode) {
+            return false;
+        }
+
+        if (!this.firstCard) {
+            this.firstCard = card;
+            this.firstCard.flipped = true;
+            return true;
+        } else {
+            this.secondCard = card;
+            this.lockMode = true;
+            this.secondCard.flipped = true;
+            return true;
+        }
+
+    },
+
+    checkMatch : function () {
+        if (!this.firstCard || !this.secondCard){
+            return false;
+        }
+        return (this.firstCard.icon === this.secondCard.icon)
+    },
+
+    clearCards : function () {
+        this.firstCard = null;
+        this.secondCard = null;
+        this.lockMode = false;
+    },
+
+    unflipCards(){
+        this.firstCard.flipped = false;
+        this.secondCard.flipped = false;
+        this.clearCards();
+    },
+
+
+    checkGameOver() {
+        return this.cards.filter(card => !card.flipped).length == 0;
+    },
 
     creatCardsFromTechs : function() {
         this.cards = [];
